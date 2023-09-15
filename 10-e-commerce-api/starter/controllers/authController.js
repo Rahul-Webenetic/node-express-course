8e6aca0f4a83adeca6e668a9904c47e9186d6056
Rahul-Webenetic <rahul.chaurasiya@webenetic.com>
2023-09-15 19:35:33 +0530

    Route setup

---
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -0,0 +1,13 @@
+const register = async (req, res) => {
+  res.send('some string value')
+}
+
+const login = async (req, res) => {
+  res.send('some string value')
+}
+
+const logout = async (req, res) => {
+  res.send('some string value')
+}
+
+module.exports = { register, login, logout }
